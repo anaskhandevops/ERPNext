3b66f58a2b43baf10b06da00a2224b591c27a4c6
Anas Khan <105498424+1DevOps2@users.noreply.github.com>
2023-11-12 03:07:33 +0500

    updating changes

--- startup-script/setup-erp.sh
+++ startup-script/setup-erp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -x
 # Define variables
 FRAPPE_BENCH_DIR="/home/frappe/frappe-bench"
 SITE_NAME="erp.net"
@@ -9,21 +9,20 @@ MARIADB_ROOT_PASSWORD='1QWERT2'
 DB_HOST='erpdb'
 
 # Move to Frappe Bench directory
-cd $FRAPPE_BENCH_DIR && \
+cd $FRAPPE_BENCH_DIR 
 
 # Create a new site
-bench new-site $SITE_NAME --admin-password $ADMIN_PASSWORD --mariadb-root-username $MARIADB_ROOT_USERNAME --mariadb-root-password $MARIADB_ROOT_PASSWORD --db-host $DB_HOST && \
+bench new-site $SITE_NAME --admin-password $ADMIN_PASSWORD --mariadb-root-username $MARIADB_ROOT_USERNAME --mariadb-root-password $MARIADB_ROOT_PASSWORD --db-host $DB_HOST 
 
 # Install ERPNext app
-bench --site $SITE_NAME install-app erpnext && \
+bench --site $SITE_NAME install-app erpnext 
 
 # Install POS-Awesome app
-bench get-app branch version-14 https://github.com/yrestom/POS-Awesome.git && \
-bench setup requirements && \
-bench build --app posawesome && \
-bench --site $SITE_NAME install-app posawesome && \
+bench get-app branch version-14 https://github.com/yrestom/POS-Awesome.git 
+bench setup requirements 
+bench build --app posawesome 
+bench --site $SITE_NAME install-app posawesome 
 
 # Start the app
-cd $FRAPPE_BENCH_DIR/sites && echo $SITE_NAME > currentsite.txt && \
+cd $FRAPPE_BENCH_DIR/sites && echo $SITE_NAME > currentsite.txt 
 cd $FRAPPE_BENCH_DIR && bench start
-
